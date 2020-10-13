40f9354abbc79ed8d0fdabc93abbdd537679065f
Al-fariqy(qywok) <59132829+belajarqywok@users.noreply.github.com>
2020-10-13 16:30:57 +0700

    Add files via upload

---
+++ 1. type data string.ts
@@ -0,0 +1,8 @@
+// [ latihan typescript ke-1 ]
+let namaSya:String="qywok";
+// error jika
+// namaSya=13;
+// namaSya=true;
+// peraturan : deklarasi type data tidak boleh beda
+
+console.log("nama saya adalah "+namaSya);
